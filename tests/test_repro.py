640676e628dc9b32f1e6b28852e733a39aea1d35
Antonio Russo <arusso@sandia.gov>
2022-03-23 13:11:40 -0600

    Always test all wvf files
    Signed-off-by: Antonio Russo <arusso@sandia.gov>

--- tests/test_repro.py
+++ tests/test_repro.py
@@ -1,19 +1,34 @@
-import unittest, os
+import os
 from pathlib import Path
 
+import unittest, pytest
+from jaqalpaw.compiler.jaqal_compiler import CircuitCompiler
 
-class ReproductionTester(unittest.TestCase):
-    def test_jaqal(self):
-        from jaqalpaw.compiler.jaqal_compiler import CircuitCompiler
 
-        examples_dir = Path("examples")
+examples_dir = Path("examples")
 
-        for filename in os.listdir(examples_dir):
-            filename = examples_dir / Path(filename)
-            if not filename.suffix == ".wvf":
-                continue
 
-            cc = CircuitCompiler(file=filename.with_suffix(".jql"))
-            cc.compile()
-            code = b"".join(qqq for q in cc.bytecode(0xFF) for qq in q for qqq in qq)
-            self.assertEqual(code, open(filename, "rb").read())
+def write_test_wvf_file(filename):
+    def inner(self):
+        cc = CircuitCompiler(file=filename.with_suffix(".jaqal"))
+        cc.compile()
+        code = b"".join(qqq for q in cc.bytecode(0xFF) for qq in q for qqq in qq)
+        self.assertEqual(code, open(filename, "rb").read())
+
+    return inner
+
+
+def make_tests(d):
+    for fname in os.listdir(d):
+        fname = d / fname
+        testname = fname.with_suffix("").name
+        if fname.is_dir():
+            yield from ((f"{testname}_{a}", b) for (a, b) in make_tests(fname))
+        elif fname.suffix == ".wvf":
+            yield (testname, write_test_wvf_file(fname))
+
+
+class TestReproduceWaveform(unittest.TestCase):
+    for testname, obj in make_tests(examples_dir):
+        exec(f"test_{testname}_wvf = obj")
+        del testname, obj
