a401dd8548ac51a36f554107f5407c5ffede16e4
Antonio Russo <arusso@sandia.gov>
2023-03-03 10:52:17 -0700

    Default to co-propagating R gates
    Signed-off-by: Antonio Russo <arusso@sandia.gov>

--- src/qscout/v1/QSCOUTBuiltins.py
+++ src/qscout/v1/QSCOUTBuiltins.py
@@ -520,4 +520,7 @@ class QSCOUTBuiltins(
     StandardJaqalGates,
     Macros,
 ):
-    pass
+    # UtilityPulses defines gate_RCoIA, and StandardJaqalGates uses
+    # gate_R to provide all the other single-qubit Jaqal Gates.
+    def gate_R(self, channel, phase, angle):
+        return self.gate_RCoIA(channel, angle=angle, phase=phase)
