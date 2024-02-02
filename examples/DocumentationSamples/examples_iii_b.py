652ab28a7a0de64c0e8cac4078f2c8b65115e428
Daniel Lobser <dlobser@sandia.gov>
2024-02-02 01:10:15 -0700

    Update examples to work with compiler updates
    Mainly adding prepare_all/measure_all. However, we could use subcircuit
    here, which I did in examples_iii_a.jaqal. I also offset the qubit index
    in this example to avoid duplicate channel 0 data, which is used for the
    global beam. Since qubit mappings are treated as a calibration parameter
    and the default output is really hardware channel it's probably easiest
    to just move the index up by one instead of worrying too much about it
    (these are toy examples, and we don't want to add unnecessary
    complication..)
    
    Also, pulse definitions for frame rotation examples were modified such
    that they produce a reasonable output in all cases (instead of just zero
    for most of them) since frame forwarding is required for output to show
    up in z0/z1 when plotting the output.

--- examples/DocumentationSamples/examples_iii_b.py
+++ examples/DocumentationSamples/examples_iii_b.py
@@ -1,13 +1,20 @@
 from jaqalpaw.ir.pulse_data import PulseData
 from qscout.v1.QSCOUTBuiltins import QSCOUTBuiltins
 
+# Note: Some gate definitions differ from the examples in the manual, in which
+# fwd_frame0_mask has been added as an argument. This is because
+# fwd_frame0_mask is needed if you want to see the applied phase from the
+# frame, instead of the frame's internal value, when using jaqalpaw-emulate.
+# Since frame forwarding requirements differ based on the gate type,
+# jaqalpaw-emulate displays applied phase during a gate, as it is more useful
+# for debugging.
 
 class ExamplesIIIBGatePulses(QSCOUTBuiltins):
     def gate_G10A(self, qubit):
-        return [PulseData(qubit, 1e-6, framerot0=10) for _ in range(3)]
+        return [PulseData(qubit, 1e-6, framerot0=10, fwd_frame0_mask=0b01) for _ in range(3)]
 
     def gate_G10B(self, qubit):
-        return [PulseData(qubit, 3e-6, framerot0=[10, 10, 10])]
+        return [PulseData(qubit, 3e-6, framerot0=[10, 10, 10], fwd_frame0_mask=0b01)]
 
     def gate_G11(self, qubit):
         return [
@@ -24,16 +31,16 @@ class ExamplesIIIBGatePulses(QSCOUTBuiltins):
 
     def gate_G12(self, qubit):
         return [
-            PulseData(qubit, 1e-6, framerot0=10, apply_at_end_mask=1),
-            PulseData(qubit, 1e-6),  # NOP
-            PulseData(qubit, 1e-6, framerot0=-5, rst_frame_mask=1),
+            PulseData(qubit, 1e-6, framerot0=10, apply_at_end_mask=1, fwd_frame0_mask=0b01),
+            PulseData(qubit, 1e-6, fwd_frame0_mask=0b01),  # NOP
+            PulseData(qubit, 1e-6, framerot0=-5, rst_frame_mask=1, fwd_frame0_mask=0b01),
         ]
 
     def gate_G13(self, qubit):
         return [
-            PulseData(qubit, 1e-6, framerot0=15),
-            PulseData(qubit, 3e-6, framerot0=(0, 10, -10, -5)),
-            PulseData(qubit, 1e-6),
+            PulseData(qubit, 1e-6, framerot0=15, fwd_frame0_mask=0b01),
+            PulseData(qubit, 3e-6, framerot0=(0, 10, -10, -5), fwd_frame0_mask=0b01),
+            PulseData(qubit, 1e-6, fwd_frame0_mask=0b01),
         ]  # NOP
 
 
